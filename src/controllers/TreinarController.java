97ef83c456871e1778fc9db1a9faeb040fd69598
Jeanderson Silva Lopes <jeananimax@gmail.com>
2019-11-19 00:52:59 -0200

    Thread no treinar

--- src/controllers/TreinarController.java
+++ src/controllers/TreinarController.java
@@ -10,6 +10,7 @@ import br.jeanderson.enums.DialogType;
 import br.jeanderson.util.DialogFX;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.input.MouseEvent;
@@ -28,13 +29,26 @@ public class TreinarController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    }
 
     @FXML
     private void actionTreinar(MouseEvent event) {
-        Treinamento t = new Treinamento();
-        t.treinar();
-        DialogFX.showMessage("Classificadores foram treinados com sucesso.", "Treinados", DialogType.SUCESS);
+        Treinamento treinamento = new Treinamento();
+        Thread t = new Thread(() -> {
+            try {
+                treinamento.treinar();
+                Platform.runLater(() -> {
+                    DialogFX.showMessage("Classificadores foram treinados com sucesso.", "Treinados", DialogType.SUCESS);
+                });
+            } catch (Exception ex) {
+                Platform.runLater(() -> {
+                    DialogFX.showMessage("Erro", "Houve um erro: " + ex.getMessage(), DialogType.ERRO);
+                });
+            }
+        });
+        t.setDaemon(true);
+        t.start();
+
     }
-    
+
 }
